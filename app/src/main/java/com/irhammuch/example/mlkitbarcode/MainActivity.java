cf4e7e2c044da74afbdf2c11f5ae7084a9d9bba6
Irham <irham@example.com>
2022-01-30 12:00:10 +0700

    bind camera preview

--- app/src/main/java/com/irhammuch/example/mlkitbarcode/MainActivity.java
+++ app/src/main/java/com/irhammuch/example/mlkitbarcode/MainActivity.java
@@ -2,22 +2,37 @@ package com.irhammuch.example.mlkitbarcode;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.util.concurrent.ExecutionException;
+
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MLKit Barcode";
     private static final int PERMISSION_CODE = 1001;
     private static final String CAMERA_PERMISSION = Manifest.permission.CAMERA;
+    private PreviewView previewView;
+    private CameraSelector cameraSelector;
+    private ProcessCameraProvider cameraProvider;
+    private Preview previewUseCase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        previewView = findViewById(R.id.previewView);
     }
 
     @Override
@@ -55,6 +70,53 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setupCamera() {
+        final ListenableFuture<ProcessCameraProvider> cameraProviderFuture =
+                ProcessCameraProvider.getInstance(this);
 
+        int lensFacing = CameraSelector.LENS_FACING_BACK;
+        cameraSelector = new CameraSelector.Builder().requireLensFacing(lensFacing).build();
+
+        cameraProviderFuture.addListener(() -> {
+            try {
+                cameraProvider = cameraProviderFuture.get();
+                bindAllCameraUseCases();
+            } catch (ExecutionException | InterruptedException e) {
+                Log.e(TAG, "cameraProviderFuture.addListener Error", e);
+            }
+        }, ContextCompat.getMainExecutor(this));
+    }
+
+    private void bindAllCameraUseCases() {
+        if (cameraProvider != null) {
+            cameraProvider.unbindAll();
+            bindPreviewUseCase();
+        }
+    }
+
+    private void bindPreviewUseCase() {
+        if (cameraProvider == null) {
+            return;
+        }
+
+        if (previewUseCase != null) {
+            cameraProvider.unbind(previewUseCase);
+        }
+
+        Preview.Builder builder = new Preview.Builder();
+        builder.setTargetRotation(getRotation());
+
+        previewUseCase = builder.build();
+        previewUseCase.setSurfaceProvider(previewView.getSurfaceProvider());
+
+        try {
+            cameraProvider
+                    .bindToLifecycle(this, cameraSelector, previewUseCase);
+        } catch (Exception e) {
+            Log.e(TAG, "Error when bind preview", e);
+        }
+    }
+
+    protected int getRotation() throws NullPointerException {
+        return previewView.getDisplay().getRotation();
     }
 }
