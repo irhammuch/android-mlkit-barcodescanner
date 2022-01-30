4c12a8ac678e4179aa0ef22f12636ed5eb43f484
Irham <irham@example.com>
2022-01-30 11:49:37 +0700

    request camera permission

--- app/src/main/java/com/irhammuch/example/mlkitbarcode/MainActivity.java
+++ app/src/main/java/com/irhammuch/example/mlkitbarcode/MainActivity.java
@@ -1,14 +1,60 @@
 package com.irhammuch.example.mlkitbarcode;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int PERMISSION_CODE = 1001;
+    private static final String CAMERA_PERMISSION = Manifest.permission.CAMERA;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        startCamera();
+    }
+
+    public void startCamera() {
+        if(ContextCompat.checkSelfPermission(this, CAMERA_PERMISSION) == PackageManager.PERMISSION_GRANTED) {
+            setupCamera();
+        } else {
+            getPermissions();
+        }
+    }
+
+    private void getPermissions() {
+        ActivityCompat.requestPermissions(this, new String[]{CAMERA_PERMISSION}, PERMISSION_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, int[] grantResults) {
+        for (int r : grantResults) {
+            if (r == PackageManager.PERMISSION_DENIED) {
+                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+                return;
+            }
+        }
+
+        if (requestCode == PERMISSION_CODE) {
+            setupCamera();
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    private void setupCamera() {
+
     }
 }
