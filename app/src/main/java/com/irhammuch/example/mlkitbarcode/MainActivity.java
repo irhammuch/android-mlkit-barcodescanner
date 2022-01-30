dd98b93183663fa309beed87e7a74f93636ab8fa
Irham <irham@example.com>
2022-01-30 12:17:23 +0700

    add barcode scan analysis use case

--- app/src/main/java/com/irhammuch/example/mlkitbarcode/MainActivity.java
+++ app/src/main/java/com/irhammuch/example/mlkitbarcode/MainActivity.java
@@ -3,6 +3,8 @@ package com.irhammuch.example.mlkitbarcode;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageProxy;
 import androidx.camera.core.Preview;
 import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.camera.view.PreviewView;
@@ -10,16 +12,24 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
 import com.google.common.util.concurrent.ListenableFuture;
+import com.google.mlkit.vision.barcode.BarcodeScanner;
+import com.google.mlkit.vision.barcode.BarcodeScanning;
+import com.google.mlkit.vision.barcode.common.Barcode;
+import com.google.mlkit.vision.common.InputImage;
 
+import java.util.List;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
     private static final String TAG = "MLKit Barcode";
     private static final int PERMISSION_CODE = 1001;
     private static final String CAMERA_PERMISSION = Manifest.permission.CAMERA;
@@ -27,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
     private CameraSelector cameraSelector;
     private ProcessCameraProvider cameraProvider;
     private Preview previewUseCase;
+    private ImageAnalysis analysisUseCase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -90,6 +101,7 @@ public class MainActivity extends AppCompatActivity {
         if (cameraProvider != null) {
             cameraProvider.unbindAll();
             bindPreviewUseCase();
+            bindAnalysisUseCase();
         }
     }
 
@@ -116,7 +128,56 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private void bindAnalysisUseCase() {
+        if (cameraProvider == null) {
+            return;
+        }
+
+        if (analysisUseCase != null) {
+            cameraProvider.unbind(analysisUseCase);
+        }
+
+        Executor cameraExecutor = Executors.newSingleThreadExecutor();
+
+        ImageAnalysis.Builder builder = new ImageAnalysis.Builder();
+        builder.setTargetRotation(getRotation());
+
+        analysisUseCase = builder.build();
+        analysisUseCase.setAnalyzer(cameraExecutor, this::analyze);
+
+        try {
+            cameraProvider
+                    .bindToLifecycle(this, cameraSelector, analysisUseCase);
+        } catch (Exception e) {
+            Log.e(TAG, "Error when bind analysis", e);
+        }
+    }
+
     protected int getRotation() throws NullPointerException {
         return previewView.getDisplay().getRotation();
+    }
+
+    @SuppressLint("UnsafeOptInUsageError")
+    private void analyze(@NonNull ImageProxy image) {
+        if (image.getImage() == null) return;
+
+        InputImage inputImage = InputImage.fromMediaImage(
+                image.getImage(),
+                image.getImageInfo().getRotationDegrees()
+        );
+
+        BarcodeScanner barcodeScanner = BarcodeScanning.getClient();
+
+        barcodeScanner.process(inputImage)
+                .addOnSuccessListener(this::onSuccessListener)
+                .addOnFailureListener(e -> Log.e(TAG, "Barcode process failure", e))
+                .addOnCompleteListener(task -> image.close());
+    }
+
+    private void onSuccessListener(List<Barcode> barcodes) {
+        if (barcodes.size() > 0) {
+            Toast.makeText(this, barcodes.get(0).getDisplayValue(), Toast.LENGTH_SHORT)
+                    .show();
+        }
     }
 }
